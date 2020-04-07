9bf37445016e7b2b92549762ea94e7f5f0abf8e6
Gaku Sasaki <gaku.sasaki@gmail.com>
2020-04-06 22:37:52 -0700

    #5~11

--- Program.cs
+++ Program.cs
@@ -22,7 +22,46 @@ namespace Basic_13
             Console.WriteLine("------------------------------");
             Console.WriteLine("Task Four:");
             int[] intArray = {1,2,3,4,5};
-            LoopArray(intArray);
+            // LoopArray(intArray);
+            
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Task Five:");
+            // FindMax(intArray);
+            
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Task Six:");
+            // GetAverage(intArray);
+            
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Task Seven:");
+            // OddArray();
+            
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Task Eight:");
+            // Console.WriteLine(GreaterThanY(intArray,3));
+            
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Task Nine:");
+            // Console.WriteLine("Before: " + intArray[1]);
+            // SquareArrayValues(intArray);
+            // Console.WriteLine("After: " + intArray[1]);
+            
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Task Ten:");
+            // int[] testTen = {1,-2,3,-4,5};
+            // Console.WriteLine("Before: " + testTen[1] + " expecting -2");
+            // EliminateNegatives(testTen);
+            // Console.WriteLine("After: " + testTen[1] + " expecting 0");
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Task Eleven:");
+            int[] testEleven = {1,-2,3,-4,5};
+            Console.WriteLine("Expecting max:5, min:-4, avg:0.66");
+            MinMaxAverage(testEleven);
+            
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("Task Twelve:");
+            
         }
 
         public static void PrintNumbers()
@@ -46,11 +85,6 @@ namespace Basic_13
         public static void PrintSum()
         // 3: Print 1-255, but with sum as you go
         {
-            // For example, your output should be something like this:
-            
-            // New number: 0 Sum: 0
-            // New number: 1 Sum: 1
-            // New Number: 2 Sum: 3
             int sum = 0;
             for(int i = 1; i <= 255; i++)
             {
@@ -71,13 +105,110 @@ namespace Basic_13
         public static int FindMax(int[] numbers)
         // 5: iterate int[] and print/return max value
         {
-            int max = 0;
+            if (numbers.Length == 0) {return 0;}
+
+            int max = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > max){
+                    max = number;
+                }
+            }
+            Console.WriteLine(max);
+            return max;
+        }
+        
+        public static int GetAverage(int[] numbers)
+        // 6: iterate int[] and print/return AVG value
+        {
+            if (numbers.Length == 0) {return 0;}
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            int avg = sum / numbers.Length;
+            Console.WriteLine(avg);
+            return avg;
+        }
+    
+        public static List<int> OddArray()
+        // 7: Create array with odds 1~255
+        // *** Will use list instead of array, since the size will need to be dynamic. Might be wrong ***
+        {
+            List<int> intList = new List<int>();
+            for (int i = 1; i <= 255; i++)
+            {
+                if (i % 2 != 0){
+                    intList.Add(i);
+                }
+            }
+            return intList;
+        }
+
+        public static int GreaterThanY(int[] numbers, int y)
+        // 8: Given int array and int y, return # of array vals greater than y
+        {
+            int answer = 0;
             foreach (var number in numbers)
             {
-                Console.WriteLine(number);
+                if (number > y){
+                    answer ++;
+                }
             }
             return answer;
         }
+
+        public static void SquareArrayValues(int[] numbers)
+        // 9: Given int array, multiply each val by itself
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = numbers[i] * numbers[i];
+            }
+            return;
+        }
+    
+        public static void EliminateNegatives(int[] numbers)
+        // 10: Given int array, replace numbers < 0 with 0
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < 0){
+                    numbers[i] = 0;
+                }
+            }
+            return;
+        }
+
+        public static void MinMaxAverage(int[] numbers)
+        // 11: Given int array, print max, min, and avg
+        {
+            if (numbers.Length == 0) {return;}
+
+            int sum = 0;
+            int max = numbers[0];
+            int min = numbers[0];
+            foreach (int number in numbers)
+            {
+                sum += number;
+                if (number > max)
+                {
+                    max = number;
+                }
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+            double avg = (double) sum / numbers.Length;
+            Console.WriteLine($"max:{max}, min:{min}, avg:{avg}");
+            return;
+        }
+    
+
+    
     
     }
 }
